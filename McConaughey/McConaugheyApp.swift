09d5a3bdf85c1519f9125463b011c64f1106a6f0
Aimee Hong <honglp@bc.edu>
2024-10-21 16:40:29 -0400

    Initial Commit

---
+++ McConaughey/McConaugheyApp.swift
@@ -0,0 +1,17 @@
+//
+//  McConaugheyApp.swift
+//  McConaughey
+//
+//  Created by Aimee Hong on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct McConaugheyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
